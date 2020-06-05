ebb80c3d84d5974bba00e22606755b0eb14e9555
OverLord <overlord@localhost.localdomain>
2020-06-05 11:36:46 +0700

    homework

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\User;
+
+class UsersTableSeeder extends Seeder
+
+{
+
+    /**
+
+     * Run the database seeds.
+
+     *
+
+     * @return void
+
+     */
+
+    public function run()
+
+    {
+
+        //tao moi admin
+
+        $user = new User();
+
+        $user->name = "adminUser";
+
+        $user->email = "admin@gmail.com";
+
+        $user->password = bcrypt("admin");
+
+        $user->admin = 1;
+
+        $user->save();
+
+        //tao user binh thuong
+
+        $user = new User();
+
+        $user->name = "user";
+
+        $user->email = "user@gmail.com";
+
+        $user->password = bcrypt("user");
+
+        $user->admin = 0;
+
+        $user->save();
+
+    }
+
+}
